5e8b93bb6c686f7ae606b25d21ad399e8715bf85
zero77787 <rsokolov995@gmail.com>
2022-12-08 23:52:39 +0300

    Домашнее задание № 5 вариант 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         Task1();
         Task2();
         Task3();
+        Task4();
+        Task5();
+        Task6();
+        Task7();
 
     }
 
@@ -33,5 +37,54 @@ public class Main {
         String fullName = lastName + firstName + midlleName;
         fullName = fullName.replace("ё", "е");
         System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+    public static void Task4() {
+        System.out.println("Задача 4");
+        Scanner name1 = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = name1.next();
+        {
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+
+    public static void Task5() {
+        System.out.println("Задача 5");
+        Scanner t = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int t1 = t.nextInt();
+        {
+            System.out.println("Температура воздуха сегодня: " + t1 + " градусов");
+        }
+    }
+    public static void Task6() {
+
+        System.out.println("Задача 6");
+        Scanner day = new Scanner(System.in);
+        System.out.println("Введите день в формате ДД ");
+        int d = day.nextInt();
+        Scanner month = new Scanner(System.in);
+        System.out.println("Введите месяц в формате ММ ");
+        int m = month.nextInt();
+        Scanner year = new Scanner(System.in);
+        System.out.println("Введите день в формате ГГГГ ");
+        int y = year.nextInt();
+        String dayMonthYear = (d + "." + m + "." + y);
+        System.out.println(dayMonthYear);
+    }
+    public static void Task7() {
+        System.out.println("Задача 6");
+        Scanner userName = new Scanner(System.in);
+        System.out.println("Введите имя пользователя: ");
+        String uN = userName.nextLine();
+        Scanner assistantName = new Scanner(System.in);
+        System.out.println("Введите имя помощника: ");
+        String aN = assistantName.nextLine();
+        Scanner messadge = new Scanner(System.in);
+        System.out.println("Введите количество сообщений: ");
+        String mes = messadge.nextLine();
+        String finalMessadge = ("Привет, " +uN+" ! "+ "Это твой помощник "+aN+" . "+ "У тебя "+ mes+ " новых писем.");
+        System.out.println(finalMessadge);
+
     }
 }
